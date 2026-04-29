b01b583102bd8c1ce9072084e50b064f2330d5c7
Shohanul Islam <shohanulislam19892@gmail.com>
2026-04-30 04:41:22 +0600

    register device view added in admin panel

--- notifications/admin.py
+++ notifications/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Notification, NotificationDevice
 
-# Register your models here.
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('title', 'recipient', 'notification_type', 'is_read', 'created_at')
+    list_filter = ('notification_type', 'is_read', 'created_at')
+    search_fields = ('title', 'message', 'recipient_object_id')
+    readonly_fields = ('created_at',)
+
+@admin.register(NotificationDevice)
+class NotificationDeviceAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'registration_id_short', 'active', 'created_at')
+    list_filter = ('active', 'created_at')
+    search_fields = ('name', 'registration_id', 'object_id')
+    readonly_fields = ('created_at',)
+
+    def registration_id_short(self, obj):
+        return obj.registration_id[:30] + '...' if len(obj.registration_id) > 30 else obj.registration_id
+    registration_id_short.short_description = 'Registration ID'
